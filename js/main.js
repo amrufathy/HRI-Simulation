b3c8d34b6668448243d569e49bf50263b89f4ccf
sheikh <ahmedhelsheikh@gmail.com>
2020-09-09 03:03:10 +0200

    Style the main page, Adding TTS

---
+++ js/main.js
@@ -0,0 +1,25 @@
+button.onclick = () => {
+    console.log('start script');
+    // button.disabled = true;
+    var msg = document.getElementById('fname').value;
+    if (msg == "") {
+         msg = 'Please enter some text';
+    }
+    const utt = new SpeechSynthesisUtterance(msg);
+    // Prevent garbage collection of utt object
+    console.log(utt);
+
+    utt.addEventListener('end', () => {
+        console.log('end event triggered');
+    });
+
+    // just for debugging completeness, no errors seem to be thrown though
+    utt.addEventListener('error', (err) => {
+        console.log('err', err)
+    });
+
+    speechSynthesis.speak(utt);
+    setTimeout(() => {
+        console.log('finished?');
+    }, 100);
+};
